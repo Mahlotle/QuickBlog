6a8ca4bfa311959e65a8bdf87348ce2539cb93f9
Mahlotle <137900766+Mahlotle@users.noreply.github.com>
2025-09-13 15:40:26 +0200

    BlogCrards

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-import { blogCategories } from "../assets/assets";
-import {motion} from "motion/react"
+import { blog_data, blogCategories } from "../assets/assets";
+import { motion } from "framer-motion";
+import BlogCard from "./BlogCard";
 
 const BlogList = () => {
   const [menu, setMenu] = useState("All");
 
   return (
     <div>
+      {/* --- Category Buttons --- */}
       <div className="flex justify-center gap-4 sm:gap-8 my-10 relative">
         {blogCategories.map((item) => (
           <div key={item} className="relative">
@@ -15,19 +17,26 @@ const BlogList = () => {
               className={`relative cursor-pointer text-gray-500 px-4 py-1 rounded-full transition-colors duration-300
                 ${menu === item ? "text-white" : ""}`}
             >
-              {/* Background circle only when active */}
               {menu === item && (
-                <motion.div 
-                  layoutId = 'underline'
-                  transition = {{type: 'spring', stiffness: 350, damping: 30}}
-                  className="absolute inset-0 -z-10 bg-primary rounded-full">
-
-                </motion.div>
+                <motion.div
+                  layoutId="underline"
+                  transition={{ type: "spring", stiffness: 350, damping: 30 }}
+                  className="absolute inset-0 -z-10 bg-primary rounded-full"
+                />
               )}
               {item}
             </button>
           </div>
         ))}
+      </div>
+
+      {/* --- Blog Cards --- */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">
+        {blog_data
+          .filter((blog) => (menu === "All" ? true : blog.category === menu))
+          .map((blog) => (
+            <BlogCard key={blog._id} blog={blog} />
+          ))}
       </div>
     </div>
   );
